57b3c3301629a717e7097044aa3432b9f91394cc
CHELIO1902 <chelio19022012@hotmail.com>
2022-05-19 20:25:12 -0500

    actualizacion pokedex

--- src/js/index.js
+++ src/js/index.js
@@ -1,97 +1,84 @@
-// variable con objeto vacio para almacenar el JSON
-let allPokemon = []
+const pokemonContainer = document.querySelector('#container');
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+let offset = 1;
+let limit = 9;
+
+previous.addEventListener('click', () => {
+    if (offset != 1) {
+        offset -= 10;
+        removeChildNodes(pokemonContainer);
+        fetchPokemons(offset, limit);
+    }
+});
+
+next.addEventListener('click', () => {
+    offset += 10;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset, limit);
+});
+
+function createPokemon(pokemon) {
+    const card = document.createElement("div");
+    card.classList.add("card", "m-2", "col-md-2");
+    const spriteContainer = document.createElement("div");
+    spriteContainer.classList.add("img-container");
+
+    const sprite = document.createElement("img");
+    sprite.src = pokemon.sprites.front_default;
+
+    const number = document.createElement('p');
+    number.textContent = pokemon.id
+
+    const weight = document.createElement('p');
+    weight.textContent = pokemon.weight
 
 
-// sirve para leer el JSON
-fetch('./src/JSON/pokedex.json')
-  .then(response => response.json())
-  .then(json => allPokemon = json)
-  .then(()=> imprimirPokemon(allPokemon) )
-  
-  // console.log(pokemon)
-
-// con esta variable seleccionamos con la clase el contenedor en el html 
-let cards = document.querySelector("#pokemons-cards");
-/* para que el formulario de entrada tenga un detector de eventos adjunto, usaré un querySelector para recuperar el botón por su nombre de clase y establecerlo en una constante (para que pueda usarse más adelante). */
-let buscandoNombres = document.querySelector("form");
-
-/* en esta variable estoy utilizando de nuevo la variable buscandoNombres declarada para obtener los datos del input */
-buscandoNombres.addEventListener("submit", (event) => {
-  //con prevent default cancelamos el evento del submit de la función
-  event.preventDefault()
-  console.log(event.target.pokemon.value)
-  console.log(allPokemon)
-  let searching = allPokemon.filter(({ name }) =>
-    name.toLocaleLowerCase().includes(event.target.pokemon.value.toLocaleLowerCase())
-  );
-  cards.innerHTML = "";
-  imprimirPokemon(searching)
-})
-
-// funcion para crear las cards y las clases de las mismas para mostrar la informacion de JSON
-let imprimirPokemon = (pokemons) => {
-  pokemons.forEach((element) => {
-    // creando los elementos del apendChild
-    let bodyCard = document.createElement("div");
-    let row = document.createElement("div");
-    let colLeft = document.createElement("div");
-    let image = document.createElement("img");
-    let colRight = document.createElement("div");
-    let infoCard = document.createElement("div");
-    let pokemonName = document.createElement("h5")
-    let infoPokemon = document.createElement("p")
-    let popover = document.createElement("a")
-    // bodyCard.setAttribute("style", "max-width: 540px;" )
-    image.setAttribute("src", element.ThumbnailImage);
-    // popover attributs
-    popover.setAttribute("tabindex", "0");
-    popover.setAttribute("role", "button");
-    popover.setAttribute("data-bs-toggle", "popover");
-    popover.setAttribute("data-bs-trigger", "focus");
-    popover.setAttribute("tittle", "Mas cacracteristicas");
-    popover.setAttribute("data-html", "true");
-    popover.setAttribute("data-content", element.name);
+    const abilities = document.createElement('p');
+    abilities.textContent = pokemon.abilities.name
 
 
-   
-// agregando las clases a los div
-    bodyCard.classList.add("card", "m-2", "col-md-3"); 
-    colLeft.classList.add("col");
-    colRight.classList.add("col-md-8");
-    infoCard.classList.add("card-body");
-    pokemonName.classList.add("card-title");
-    infoPokemon.classList.add("card-text");
-    popover.classList.add("btn", "btn-sm", "btn-danger");
 
-// agregamos la informacion a las etiquetas de texto
-    pokemonName.innerText = element.number;
-    infoPokemon.innerText = element.name;
-    popover.innerText = "Ver Mas...";
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.textContent = pokemon.name;
 
-// agregando los div con apendChild
-    cards.appendChild(bodyCard);
-    bodyCard.appendChild(row);
-    row.appendChild(colLeft);
-    colLeft.appendChild(image);
-    row.appendChild(colRight);
-    colRight.appendChild(infoCard);
-    infoCard.appendChild(pokemonName);
-    infoCard.appendChild(infoPokemon);
-    infoCard.appendChild(popover);
+    spriteContainer.appendChild(sprite);
+    card.appendChild(spriteContainer);
+    card.appendChild(weight);
+    card.appendChild(number);
+    card.appendChild(abilities);
+    card.appendChild(name);
 
-  });
-};
+    pokemonContainer.appendChild(card)
+        //console.log(pokemon);
+}
 
-/* var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-}) */
+function fetchPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            createPokemon(data)
+        });
 
-$('[data-bs-toggle="popover"]').popover(); 
+}
 
-/* $(document).ready(function () {
-  $('[data-toggle="popover"]').popover();
-}); */
+function fetchPokemons(offset, limit) {
+    for (let i = offset; i <= offset + limit; i++) {
+        fetchPokemon(i);
+    }
+
+}
+
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+fetchPokemons(offset, limit);
 
 
 
